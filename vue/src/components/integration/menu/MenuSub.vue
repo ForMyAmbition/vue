ade8c53ee27a2000f93e8c9b4c9b1b8c8a0ea32e
ForMyAmbition <2448778278@qq.com>
2023-12-08 00:10:44 +0800

    2023年12月8日00点09分左侧菜单雏形

---
+++ vue/src/components/integration/menu/MenuSub.vue
@@ -0,0 +1,28 @@
+<!--  -->
+<template>
+  <el-sub-menu>
+    <template v-for="(item, index) in props.menuChildren" :key="index">
+      <MenuSub v-if="item.children.length"></MenuSub>
+      <el-menu-item v-else></el-menu-item>
+    </template>
+  </el-sub-menu>
+</template>
+
+<script setup lang="ts">
+// import { PropsType } from './type';
+// import MenuItem from './MenuItem.vue'
+
+const props = defineProps({
+  menuChildren: {
+    type: Array<typeProps>,
+    default: []
+  }
+})
+
+interface typeProps {
+  title: String
+  children: Array<typeProps>
+  path: String
+}
+</script>
+<style lang="less" scoped></style>
